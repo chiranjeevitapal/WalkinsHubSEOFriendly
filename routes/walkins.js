80eee684e15a029d4ac0b826b648823f573a97af
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2017-03-10 06:26:49 +0530

    added date function

--- routes/walkins.js
+++ routes/walkins.js
@@ -176,7 +176,15 @@ router.post('/postWalkin', (req, res) => {
 })
 
 
+Date.prototype.yyyymmdd = function() {
+    var mm = this.getMonth() + 1; // getMonth() is zero-based
+    var dd = this.getDate();
 
+    return [this.getFullYear(),
+        (mm > 9 ? '' : '0') + mm,
+        (dd > 9 ? '' : '0') + dd
+    ].join('-');
+};
 
 
 
