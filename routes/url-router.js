6048d37c6eb92c2d3f044d66b2ccc8d606d8dc77
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2017-06-07 20:31:29 +0530

    removed manual handling of domain name

--- routes/url-router.js
+++ routes/url-router.js
@@ -5,7 +5,7 @@ var path = require('path');
 var walkins = require('./walkins');
 
 //Prod
-var domainName = 'walkinshub.com';
+//var domainName = 'walkinshub.com';
 //Dev
 //var domainName = 'localhost:8080';
 
@@ -27,220 +27,154 @@ module.exports = function (app) {
     app.use('/api/', walkins);
     app.get('/', (req, res) => {
         var host = req.headers.host;
-        if (host.toLowerCase().indexOf(domainName) != -1) {
-            db.collection('walkins').find({}).sort({
-                "date": -1
-            }).limit(300).toArray((err, result) => {
-                if (err) return console.log(err)
-                res.setHeader("Cache-Control", "public, max-age=86400, no-cache");
-                res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
-                res.render('home.ejs', {
-                    walkins: result,
-                    jobsType: 'Walk-ins',
-                    user: req.user
-                })
+        db.collection('walkins').find({}).sort({
+            "date": -1
+        }).limit(300).toArray((err, result) => {
+            if (err) return console.log(err)
+            res.setHeader("Cache-Control", "public, max-age=86400, no-cache");
+            res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
+            res.render('home.ejs', {
+                walkins: result,
+                jobsType: 'Walk-ins',
+                user: req.user
             })
-        } else {
-            res.writeHead(301, {
-                Location: 'http://www.walkinshub.com/'
-            });
-            res.end();
-        }
+        })
     })
 
     /* GET One Walkin with the provided ID */
     app.get('/walkin/:id/', function (req, res) {
         var host = req.headers.host;
-        if (host.toLowerCase().indexOf(domainName) != -1) {
-            //var id = req.params.id.substring(req.params.id.lastIndexOf('-') + 1);
-            var id = req.params.id;
-            //console.log(id);
-            if (id.indexOf('-') == -1) {
-                db.collection('walkins').findOne({
-                    _id: ObjectId(id)
-                }, function (err, result) {
-                    if (err) return console.log(err)
-                    if (result == null) {
-                        res.redirect('/')
-                    } else {
-                        res.setHeader("Cache-Control", "public, max-age=86400");
-                        res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
-                        res.render('details.ejs', {
-                            walkin: result,
-                            user: req.user
-                        })
-                    }
-                });
-            } else {
-                db.collection('walkins').findOne({
-                    _id: id
-                }, function (err, result) {
-                    if (err) return console.log(err)
-                    if (result == null) {
-                        res.redirect('/')
-                    } else {
-                        res.setHeader("Cache-Control", "public, max-age=86400");
-                        res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
-                        res.render('details.ejs', {
-                            walkin: result,
-                            user: req.user
-                        })
-                    }
-                });
-            }
-        } else {
-            res.writeHead(301, {
-                Location: 'http://www.walkinshub.com/'
+        //var id = req.params.id.substring(req.params.id.lastIndexOf('-') + 1);
+        var id = req.params.id;
+        //console.log(id);
+        if (id.indexOf('-') == -1) {
+            db.collection('walkins').findOne({
+                _id: ObjectId(id)
+            }, function (err, result) {
+                if (err) return console.log(err)
+                if (result == null) {
+                    res.redirect('/')
+                } else {
+                    res.setHeader("Cache-Control", "public, max-age=86400");
+                    res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
+                    res.render('details.ejs', {
+                        walkin: result,
+                        user: req.user
+                    })
+                }
             });
-            res.end();
+        } else {
+            db.collection('walkins').findOne({
+                _id: id
+            }, function (err, result) {
+                if (err) return console.log(err)
+                if (result == null) {
+                    res.redirect('/')
+                } else {
+                    res.setHeader("Cache-Control", "public, max-age=86400");
+                    res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
+                    res.render('details.ejs', {
+                        walkin: result,
+                        user: req.user
+                    })
+                }
+            });
         }
     });
 
     app.get('/jobs/fresher/', (req, res) => {
         var host = req.headers.host;
-        if (host.toLowerCase().indexOf(domainName) != -1) {
-            db.collection('walkins').find({
-                $or: [{
-                    "experience": /0 -/
-                }, {
-                    "experience": /Fresher/
-                }]
-            }).sort({
-                "date": -1
-            }).limit(300).toArray((err, result) => {
-                if (err) return console.log(err)
-                res.setHeader("Cache-Control", "public, max-age=86400");
-                res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
-                res.render('home.ejs', {
-                    walkins: result,
-                    jobsType: 'Fresher Jobs',
-                    user: req.user
-                })
+        db.collection('walkins').find({
+            $or: [{
+                "experience": /0 -/
+            }, {
+                "experience": /Fresher/
+            }]
+        }).sort({
+            "date": -1
+        }).limit(300).toArray((err, result) => {
+            if (err) return console.log(err)
+            res.setHeader("Cache-Control", "public, max-age=86400");
+            res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
+            res.render('home.ejs', {
+                walkins: result,
+                jobsType: 'Fresher Jobs',
+                user: req.user
             })
-        } else {
-            res.writeHead(301, {
-                Location: 'http://www.walkinshub.com/'
-            });
-            res.end();
-        }
+        })
     })
 
     app.get('/jobs/experienced/', (req, res) => {
         var host = req.headers.host;
-        if (host.toLowerCase().indexOf(domainName) != -1) {
-            db.collection('walkins').find({
-                $and: [{
-                    "experience": {
-                        $not: /0 -/
-                    }
-                }, {
-                    "experience": {
-                        $not: /Fresher/
-                    }
-                }]
-            }).sort({
-                "date": -1
-            }).limit(200).toArray((err, result) => {
-                if (err) return console.log(err)
-                res.setHeader("Cache-Control", "public, max-age=86400");
-                res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
-                res.render('home.ejs', {
-                    walkins: result,
-                    jobsType: 'Experienced Jobs',
-                    user: req.user
-                })
+        db.collection('walkins').find({
+            $and: [{
+                "experience": {
+                    $not: /0 -/
+                }
+            }, {
+                "experience": {
+                    $not: /Fresher/
+                }
+            }]
+        }).sort({
+            "date": -1
+        }).limit(200).toArray((err, result) => {
+            if (err) return console.log(err)
+            res.setHeader("Cache-Control", "public, max-age=86400");
+            res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
+            res.render('home.ejs', {
+                walkins: result,
+                jobsType: 'Experienced Jobs',
+                user: req.user
             })
-        } else {
-            res.writeHead(301, {
-                Location: 'http://www.walkinshub.com/'
-            });
-            res.end();
-        }
+        })
     })
 
     app.get('/jobs/:location/', function (req, res) {
         var host = req.headers.host;
 
         //var id = req.params.id.substring(req.params.id.lastIndexOf('-') + 1);
-        if (host.toLowerCase().indexOf(domainName) != -1) {
-            var location = req.params.location.toLowerCase();
-            db.collection('walkins').find({
-                location: new RegExp('^' + location + '$', 'i')
-            }).sort({
-                "date": -1
-            }).limit(200).toArray((err, result) => {
-                if (err) return console.log(err)
-                res.setHeader("Cache-Control", "public, max-age=86400");
-                res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
-                res.render('home.ejs', {
-                    walkins: result,
-                    jobsType: location + ' Jobs',
-                    user: req.user
-                })
+        var location = req.params.location.toLowerCase();
+        db.collection('walkins').find({
+            location: new RegExp('^' + location + '$', 'i')
+        }).sort({
+            "date": -1
+        }).limit(200).toArray((err, result) => {
+            if (err) return console.log(err)
+            res.setHeader("Cache-Control", "public, max-age=86400");
+            res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
+            res.render('home.ejs', {
+                walkins: result,
+                jobsType: location + ' Jobs',
+                user: req.user
             })
-        } else {
-            res.writeHead(301, {
-                Location: 'http://www.walkinshub.com/'
-            });
-            res.end();
-        }
+        })
     });
 
     app.get('/contact/', (req, res) => {
         var host = req.headers.host;
-
-        if (host.toLowerCase().indexOf(domainName) != -1) {
-            res.render('contact.ejs', {
-                user: req.user
-            })
-        } else {
-            res.writeHead(301, {
-                Location: 'http://www.walkinshub.com/'
-            });
-            res.end();
-        }
+        res.render('contact.ejs', {
+            user: req.user
+        })
     })
 
     app.get('/about/', (req, res) => {
         var host = req.headers.host;
-
-        if (host.toLowerCase().indexOf(domainName) != -1) {
-            res.render('about.ejs', {
-                user: req.user
-            })
-        } else {
-            res.writeHead(301, {
-                Location: 'http://www.walkinshub.com/'
-            });
-            res.end();
-        }
+        res.render('about.ejs', {
+            user: req.user
+        })
     })
 
     app.get('/uploadChethan/', (req, res) => {
         var host = req.headers.host;
-
-        if (host.toLowerCase().indexOf(domainName) != -1) {
-            res.render('uploadChethan.ejs');
-        } else {
-            res.writeHead(301, {
-                Location: 'http://www.walkinshub.com/'
-            });
-            res.end();
-        }
+        res.render('uploadChethan.ejs');
     })
 
     app.get('/logos/:img/', function (req, res) {
         var host = req.headers.host;
-        if (host.toLowerCase().indexOf(domainName) != -1) {
-            res.setHeader("Cache-Control", "public, max-age=86400");
-            res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
-            res.sendFile(path.join(__dirname, '../logos', req.params.img));
-        } else {
-            res.writeHead(301, {
-                Location: 'http://www.walkinshub.com/'
-            });
-            res.end();
-        }
+        res.setHeader("Cache-Control", "public, max-age=86400");
+        res.setHeader("Expires", new Date(Date.now() + 86400000).toUTCString());
+        res.sendFile(path.join(__dirname, '../logos', req.params.img));
     });
 
     app.get('/sitemap.xml', function (req, res) {
