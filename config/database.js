4fce7e0a572d50ec744c118eab98edd612eefdec
Chiranjeevi Tapal <chiranjeevi.tapal@outlook.com>
2018-05-20 13:18:50 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     //url : 'mongodb://localhost:27017/jobu'
-    url: 'mongodb://ctapal:chinna123@localhost:27113/jobu'
+    url: 'mongodb://ctapal:chinna123@localhost:27017/jobu'
 }
